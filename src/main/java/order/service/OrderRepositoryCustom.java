0774152495b8e99f3f7aaf77189caf4b9adfb0e8
sey-kh <piseykorn@gmail.com>
2019-07-09 18:14:48 +0700

    remove some non-necessary folders

--- src/main/java/order/service/OrderRepositoryCustom.java
+++ src/main/java/order/service/OrderRepositoryCustom.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface OrderRepositoryCustom {
     List<Order> findOrdersByAccountAndSymbol(String account, String symbol);
+
     Order updateQuantity(int quantity, String orderId);
+
     String createOrder(Order order);
+
     Order cancelOrder(String orderId);
 }
