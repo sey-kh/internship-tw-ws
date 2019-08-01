36d9d95826417b97f4c2f2787a057a128b54dcd2
sey-kh <piseykorn@gmail.com>
2019-08-01 14:36:24 +0700

    make an enhancement to deleteInBatch

--- src/main/java/order/constant/Consts.java
+++ src/main/java/order/constant/Consts.java
@@ -12,5 +12,4 @@ public final class Consts {
     public static final String ByTime = "bytime";
     public static final String ByOtherOrder = "byotherorder";
     public final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    public final static String Duplicated_Order = "Transaction failed due to duplicated order constraints";
 }
